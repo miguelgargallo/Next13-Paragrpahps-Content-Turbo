16b66d230d7fefd2b66258d3372a0b30983a798a
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-26 12:45:15 +0100

    1.0.2 Menus quitados

--- apps/web/src/pages/menus.tsx
+++ apps/web/src/pages/menus.tsx
@@ -2,6 +2,12 @@
 import Head from "next/head";
 import { Menu } from "ui";
 import { Menu1 } from "ui";
+import { Menu2 } from "ui";
+import { Menu3 } from "ui";
+import { Menu4 } from "ui";
+import { Menu5 } from "ui";
+import { Menu6 } from "ui";
+import { Menu7 } from "ui";
 import { Footer } from "ui";
 
 export default function Home() {
@@ -12,8 +18,14 @@ export default function Home() {
         <title>Super Menu | Sushi JS</title>
         <link rel="icon" href="./favicon.ico" />
       </Head>
-      <Menu1 />   
       <Menu />
+      <Menu1 />
+      <Menu2 />
+      <Menu3 />
+      <Menu4 />
+      <Menu5 />
+      <Menu6 />
+      <Menu7 />       
       <Footer />
     </div>
   );
